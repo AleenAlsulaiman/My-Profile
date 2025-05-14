c2c2b7a52126878e93d6a8c69eab8cac3ff14f90
AleenAlsulaiman <165071417+AleenAlsulaiman@users.noreply.github.com>
2025-05-14 15:08:35 +0300

    Delete My Profile directory

--- My Profile/script.js
+++
@@ -1,5 +0,0 @@
-document.getElementById("darkModeToggle").addEventListener("click", () => {
-  document.body.classList.toggle("dark");
-  const icon = document.getElementById("darkModeToggle");
-  icon.textContent = document.body.classList.contains("dark") ? "☀️" : "🌙";
-});
